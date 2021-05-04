7c81477872df3616b02b767063532594370ca656
phillip9807@gmail.com <phillip9807@gmail.com>
2021-05-04 20:22:38 +0900

    naverbest-final

--- webScraping3.py
+++ webScraping3.py
@@ -19,6 +19,18 @@ for index in range(0, 25):
     link = dl_data.select('a')[0].get('href')
     book_page_urls.append(link)
 
+# Extract neccessary info from metadata
+for index, book_page_url in enumerate(book_page_urls):
+    driver.get(book_page_url)
+    bsObject = BeautifulSoup(driver.page_source, 'html.parser')
 
+    title = bsObject.find('meta', {'property':'og:title'}).get('content')
+    author = bsObject.find('dt', text='저자').find_next_siblings('dd')[0].text.strip()
+    image = bsObject.find('meta', {'property':'og:image'}).get('content')
+    url = bsObject.find('meta', {'property':'og:url'}).get('content')
 
+    dd = bsObject.find('dt', text='가격').find_next_siblings('dd')[0]
+    salePrice = dd.select('div.lowest strong')[0].text
+    originalPrice = dd.select('div.lowest span.price')[0].text
 
+    print(index+1, title, author, image, url, originalPrice, salePrice)
